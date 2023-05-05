54965024800dfb33fd29b45f9d4fe12172c128aa
freznel10 <freznel@gmail.com>
2023-05-05 18:40:14 +0800

    some updates

--- keyboards/zerfstudios/chunky/chunky.c
+++ keyboards/zerfstudios/chunky/chunky.c
@@ -713,7 +713,6 @@ void housekeeping_task_kb(void) {
 const pointing_device_config_t pointing_device_configs[POINTING_DEVICE_COUNT] = {
     // {.driver = &cirque_driver_spi_default, .config = &cirque_config_spi_left, .throttle = 10, .side = LEFT},
     // {.driver = &cirque_driver_spi_default, .config = &cirque_config_spi_right, .throttle = 10, .side = RIGHT}
-    // {.driver = &ps2_trackpoint_driver_ps2_default, .config = &ps2_trackpoint_config_ps2_default, .throttle = 10, .side = LEFT},
     {.driver = &pmw3360_driver_spi_default, .config = &pmw3360_config_spi_default, .throttle = 10, .invert = INVERT_X, .side = LEFT},
       {.driver = &pmw3360_driver_spi_default, .config = &pmw3360_config_spi_default, .throttle = 10, .invert = INVERT_X, .side = RIGHT},
     // {.driver = &ps2_trackpoint_driver_ps2_default, .config = &ps2_trackpoint_config_ps2_default, .side = LEFT}
