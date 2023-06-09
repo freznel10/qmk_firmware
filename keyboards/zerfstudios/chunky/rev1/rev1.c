2667e2c03c8845f7b53eb4a0955bb71d2c3f7a27
freznel10 <freznel@gmail.com>
2023-06-09 17:46:08 +0800

    some catch up
    }

--- keyboards/zerfstudios/chunky/rev1/rev1.c
+++ keyboards/zerfstudios/chunky/rev1/rev1.c
@@ -19,7 +19,7 @@
 const pointing_device_config_t pointing_device_configs[POINTING_DEVICE_COUNT] = {
     {.driver = &pmw3360_driver_spi_default, .config = &pmw3360_config_spi_default, .throttle = 10, .invert = INVERT_X, .side = LEFT},
     {.driver = &pmw3360_driver_spi_default, .config = &pmw3360_config_spi_default, .throttle = 10, .invert = INVERT_X, .side = RIGHT},
-    // {.driver = &ps2_trackpoint_driver_ps2_default, .config = &ps2_trackpoint_config_ps2_default, .side = LEFT}
+    {.driver = &ps2_trackpoint_driver_ps2_default, .config = &ps2_trackpoint_config_ps2_default, .side = LEFT}
 
 };
 
