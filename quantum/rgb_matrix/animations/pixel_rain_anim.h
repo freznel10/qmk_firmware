8be3cc353abdc297381621304ec36612a1a1c33c
freznel10 <freznel@gmail.com>
2023-06-29 14:17:58 +0800

    stst

--- quantum/rgb_matrix/animations/pixel_rain_anim.h
+++ quantum/rgb_matrix/animations/pixel_rain_anim.h
@@ -12,8 +12,8 @@ static bool PIXEL_RAIN(effect_params_t* params) {
         return 500 / scale16by8(qadd8(rgb_matrix_config.speed, 16), 16);
     }
 
-    void rain_pixel(uint8_t i, effect_params_t * params, bool off) {
-        if (!HAS_ANY_FLAGS(g_led_config.flags[i], params->flags)) {
+    inline void rain_pixel(uint8_t led_index) {
+        if (!HAS_ANY_FLAGS(g_led_config.flags[led_index], params->flags)) {
             return;
         }
         HSV hsv = (random8() & 2) ? (HSV){0, 0, 0} : (HSV){random8(), random8_min_max(127, 255), rgb_matrix_config.hsv.v};
@@ -24,7 +24,7 @@ static bool PIXEL_RAIN(effect_params_t* params) {
 
     RGB_MATRIX_USE_LIMITS(led_min, led_max);
     if (g_rgb_timer > wait_timer) {
-        rain_pixel(random8_max(RGB_MATRIX_LED_COUNT), params, random8() & 2);
+        rain_pixel(random8_max(RGB_MATRIX_LED_COUNT));
     }
     return rgb_matrix_check_finished_leds(led_max);
 }
